8002d4a2da6023dba79e3c0940f63233d5036797
Jan-A00 <64453550+Jan-A00@users.noreply.github.com>
2020-10-02 23:26:54 +0800

    Experimented with scriptable objects
    I wouldn't say there's too many progress, I feel like I don't know what I'm doing, but hopefully i am on the right track

---
+++ TravellingStickSalesman_ProjectFolder/Assets/Scripts/Stick.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Stick", menuName = "Stick")]
+public class Stick : ScriptableObject
+{
+     public string name;
+     public string description;
+     public Sprite stickArt;
+}
