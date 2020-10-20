078a8b3808582b56727a27c0116dc8631cf8360e
TineKent <64451303+TineKent@users.noreply.github.com>
2020-10-20 23:33:42 +0800

    Added Mushroom Puzzle

---
+++ TravellingStickSalesman_ProjectFolder/Assets/Scripts/MusicalMushroomSettings.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class MusicalMushroomSettings : MonoBehaviour
+{
+    public float highightOnClickDuration;
+    public AudioClip[] mushroomAudioClips;
+    public Sprite[] mushroomSprites;
+    public Sprite[] highlightedMushroomSprites;
+    public Sprite[] wallRunes;
+    public Sprite[] highlightedWallRunes;
+    public MusicalWallRune[] runeSequence;
+    public readonly Notes[] noteArray = new Notes[8]
+    {
+        Notes.C1,
+        Notes.D,
+        Notes.E,
+        Notes.F,
+        Notes.G,
+        Notes.A,
+        Notes.B,
+        Notes.C2
+    };
+       
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private int IndexOfNote(Notes mushroomNote)
+    {
+        return Array.IndexOf(noteArray, mushroomNote);
+    }
+
+    private AudioClip AudioClipRetriever(AudioClip[] targetArrayVariable, Notes mushroomNote)
+    {
+        return targetArrayVariable[IndexOfNote(mushroomNote)];
+    }
+    private Sprite SpriteRetriever(Sprite[] targetArrayVariable, Notes mushroomNote)
+    {
+        return targetArrayVariable[IndexOfNote(mushroomNote)];
+    }
+
+    public AudioClip GetMushroomAudio(Notes mushroomNote)
+    {
+        return AudioClipRetriever(mushroomAudioClips, mushroomNote);
+    }
+
+    public Sprite GetMushroomSprite(Notes mushroomNote)
+    {
+        return SpriteRetriever(mushroomSprites, mushroomNote);
+    }
+
+    public Sprite GetMushroomHighlightSprite(Notes mushroomNote)
+    {
+        return SpriteRetriever(highlightedMushroomSprites, mushroomNote);
+    }
+
+    public Sprite GetNormalWallRune(Notes mushroomNote)
+    {
+        return SpriteRetriever(wallRunes, mushroomNote);
+    }
+
+    public Sprite GetHighlightedWallRune(Notes mushroomNote)
+    {
+        return SpriteRetriever(highlightedWallRunes, mushroomNote);
+    }   
+
+}
---
+++ TravellingStickSalesman_ProjectFolder/Assets/Scripts/Utilities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utilities
+{
+    public static bool BooleanThatLogsIfTrue(bool returnValue, string message)
+    {
+        if (returnValue)
+        {
+            Debug.Log(message);
+        }
+        return returnValue;
+    }
+}
